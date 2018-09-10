6b7e4ac327b728ad4053ec5330be04695c16e0ef
Javier G. Sogo <jgsogo@gmail.com>
2018-09-10 11:42:29 +0200

    using CMake

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
+from conans.errors import ConanException
 import os
 
 
@@ -14,7 +15,7 @@ class OpenEXRConan(ConanFile):
     generators = "cmake"
     exports = "FindOpenEXR.cmake"
 
-    requires = "ilmbase/{version}@jgsogo/testing".format(version=version)
+    # requires = "ilmbase/{version}@jgsogo/testing".format(version=version)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -24,31 +25,48 @@ class OpenEXRConan(ConanFile):
         if "fPIC" in self.options.fields and self.options.shared:
             self.options.fPIC = True
 
+        if self.settings.compiler == 'gcc' and self.settings.compiler.libcxx == 'libstdc++':
+            raise ConanException("Compile with stdlib=libstdc++11 using settings.compiler.libcxx")
+
     def source(self):
-        url = "https://github.com/openexr/openexr/releases/download/v{version}/openexr-{version}.tar.gz"
+        url = "https://github.com/openexr/openexr/archive/v{version}.tar.gz"
         tools.get(url.format(version=self.version))
+        tools.replace_in_file(os.path.join('openexr-{}'.format(self.version), 'CMakeLists.txt'),
+                              'project(OpenEXR VERSION ${OPENEXR_VERSION})',
+                              """project(OpenEXR VERSION ${OPENEXR_VERSION})
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()
+""")
 
     def build(self):
-        yes_no = {True: "enable", False: "disable"}
-        args = ["--{}-shared".format(yes_no.get(bool(self.options.shared))),
-                "--{}-static".format(yes_no.get(not bool(self.options.shared))),
-                "--{}-namespaceversioning".format(yes_no.get(bool(self.options.namespace_versioning))),
-                ]
+        cmake = CMake(self)
+        cmake.definitions["OPENEXR_BUILD_PYTHON_LIBS"] = False
+        cmake.definitions["OPENEXR_BUILD_SHARED"] = self.options.shared
+        cmake.definitions["OPENEXR_BUILD_STATIC"] = not bool(self.options.shared)
+        cmake.definitions["OPENEXR_NAMESPACE_VERSIONING"] = self.options.namespace_versioning
+        cmake.definitions["OPENEXR_ENABLE_TESTS"] = False
 
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.configure(configure_dir='openexr-{}'.format(self.version), args=args)
-        autotools.make()
-        tools.replace_prefix_in_pc_file("OpenEXR.pc", "${package_root_path_openexr}")
+        # cmake.definitions["OPENEXR_BUILD_ILMBASE"] = False
+        # cmake.definitions["ILMBASE_PACKAGE_PREFIX"] = self.deps_cpp_info["ilmbase"].rootpath
+
+        cmake.configure(source_dir='openexr-{}'.format(self.version))
+        cmake.build()
 
     def package(self):
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.install()
+        cmake = CMake(self)
+        cmake.install()
         self.copy("FindOpenEXR.cmake", src=".", dst=".")
         self.copy("license*", dst="licenses", src="ilmbase-%s" % self.version, ignore_case=True, keep_path=False)
 
     def package_info(self):
+        parsed_version = self.version.split('.')
+        version_suffix = "-%s_%s" % (
+        parsed_version[0], parsed_version[1]) if self.options.namespace_versioning else ""
+        if not self.options.shared:
+            version_suffix += "_s"
+
         self.cpp_info.includedirs = [os.path.join('include', 'OpenEXR'), ]
-        self.cpp_info.libs = ['IlmImf', 'IlmImfUtil']
+        self.cpp_info.libs = ['IlmImf' + version_suffix, 'IlmImfUtil' + version_suffix]
 
         if self.options.shared and self.settings.os == "Windows":
             self.cpp_info.defines.append("OPENEXR_DLL")
